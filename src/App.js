a7481839b32e8f29f6c298a81bb3d0b9e9e29385
AaronPerpetua <perpetua.aaron@gmail.com>
2022-02-06 02:04:04 +0800

    add changes

--- src/App.js
+++ src/App.js
@@ -1,30 +1,22 @@
-import React from 'react'
+import React from "react";
 import SignIn from "./components/SignIn";
-import Chat from './components/Chat';
-import { login } from './firebase.js';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import NavBar from './components/NavBar';
+import Chat from "./components/Chat";
+import { login } from "./firebase.js";
+import { useAuthState } from "react-firebase-hooks/auth";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import { Container} from 'react-bootstrap';
-import Products from './components/Products';
-
-
+import { Container } from "react-bootstrap";
+import Products from "./components/Products";
 
 function App() {
-
-  const [user] = useAuthState(login)
+  const [user] = useAuthState(login);
 
   return (
     <React.Fragment>
-
       <Container>
-        <NavBar/>
         {user ? <Chat /> : <SignIn />}
-       {/* <Products/> */}
+        {/* <Products/> */}
       </Container>
-     
-      
     </React.Fragment>
   );
 }
